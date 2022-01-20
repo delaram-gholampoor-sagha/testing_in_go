52172e794a32592756bf5bb2c2557cb213773d65
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2022-01-20 15:02:06 +0330

    generating mocks with vektra/mockery

--- service/sservice_test.go
+++ service/sservice_test.go
@@ -1,25 +1,25 @@
 package service_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/Delaram-Gholampoor-Sagha/testing_in_go/service"
-	"github.com/stretchr/testify/mock"
+	"github.com/Delaram-Gholampoor-Sagha/testing_in_go/service/mocks"
+	
 )
 
-type smsServiceMock struct {
-	mock.Mock
-}
+// type smsServiceMock struct {
+// 	mock.Mock
+// }
 
-func (m *smsServiceMock) SendChargeNotification(value int) bool {
-	fmt.Println("Mocked charge notification")
-	arg := m.Called(value)
-	return arg.Bool(0)
-}
+// func (m *smsServiceMock) SendChargeNotification(value int) bool {
+// 	fmt.Println("Mocked charge notification")
+// 	arg := m.Called(value)
+// 	return arg.Bool(0)
+// }
 
 func TestChargeCustomer(t *testing.T) {
-	smsService := new(smsServiceMock)
+	smsService := new(mocks.MessageService)
 	smsService.On("SendChargeNotification", 100).Return(true)
 
 	myService := service.MyService{smsService}
