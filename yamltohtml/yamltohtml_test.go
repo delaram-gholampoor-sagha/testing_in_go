301c7d407e0316ae220aa834ddae396bb89a7d06
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2022-01-20 09:41:58 +0330

    Testdata Directory for Tests

---
+++ yamltohtml/yamltohtml_test.go
@@ -0,0 +1,45 @@
+package yamltohtmlgo_test
+
+import (
+	"testing"
+
+	yamltohtmlgo "github.com/Delaram-Gholampoor-Sagha/testing_in_go/yamltohtml"
+)
+
+
+
+
+type TestCase struct {
+	desc string
+	path string
+	expected string
+}
+
+func TestYamlToHTML(t *testing.T) {
+	testCases := []TestCase{
+		TestCase{
+			desc: "Test Case 1",
+			path: "testdata/test_01.yaml",
+			expected: "<html><head><title>My Awesome Page</title></head><body>This is my fantastic content</body></html>",
+		},
+		TestCase{
+			desc: "Test Case 2",
+			path: "testdata/test_02.yaml",
+			expected: "<html><head><title>My Second Page</title></head><body>This is my fantastic content</body></html>",
+		},
+	}
+
+	for _ , test := range testCases {
+		 t.Run(test.desc , func(t *testing.T) {
+			 result , err := yamltohtmlgo.YamlToHTML(test.path)
+			 if err != nil {
+				 t.Fail()
+			 }
+			 t.Log(result)
+			 if result != test.expected {
+				 t.Fail()
+			 }
+		 })
+	}
+  
+}
